dfa97c1fb4a022b9d62d6f4940b839d539e5858c
DannyM1chael <coldsoulrusirk@gmail.com>
2020-05-12 22:39:48 +0300

    add build

---
+++ build/precache-manifest.309e06a50830aae97884df5fcbeae11c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d343c9b3d8e48f265bf2cbbcfb5e9cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "599b7df4f48a151ee3cb",
+    "url": "/static/css/main.5e14d1e7.chunk.css"
+  },
+  {
+    "revision": "a9a6f12dc7edc26ec1e6",
+    "url": "/static/js/2.d24dd7fd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.d24dd7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599b7df4f48a151ee3cb",
+    "url": "/static/js/main.db20db1e.chunk.js"
+  },
+  {
+    "revision": "b3a0b819235ff2216f72",
+    "url": "/static/js/runtime-main.123e0bd1.js"
+  },
+  {
+    "revision": "29f7b0b2722abf2acbb0e5617f48ce20",
+    "url": "/static/media/covid-logo.29f7b0b2.png"
+  }
+]);
